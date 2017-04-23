ac9340cc48bd7d52fc8b29716b12560183140043
Quan <shinichi.tk21@gmail.com>
2017-04-23 10:02:45 +0700

    Chapter8
    Chaper8
    
    Chaper8
    
    Chapter9
    
    Chapter8
    
    Chapter8

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,12 @@ class User < ApplicationRecord
   def downcase_email
     self.email = email.downcase
   end
+
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+  end
 end
