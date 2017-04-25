c8bb8d951d88decd80a6b6561d967496cb7c88f7
Quan <shinichi.tk21@gmail.com>
2017-04-25 08:22:18 +0700

    Chapter11
    Chapter11
    
    Chapter11
    
    Chapter10
    
    Chapter10
    
    Chapter11
    
    Chapter11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == "1"? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == "1"? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = t("active.notactivated") + t("active.checkemail")
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t "login.invalid"
       render :new
